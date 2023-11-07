15237bf810c50bb1b8d7c4f75e65e57c80bfcd2e
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-11-07 22:22:44 +0000

    Add bootstrap server core and update server initialization

---
+++ src/core/bootstrap-server.core.ts
@@ -0,0 +1,39 @@
+import { Hono } from 'npm:hono'
+
+interface ForInitiableServer {
+  /**
+   * @description port to listen
+   */
+  port?: number | string
+  /**
+   * @description abort controller is used to abort the server when the server is not needed
+   */
+  abortController: AbortController
+  /**
+   * @description Instace of Hono
+   * @default new Hono()
+   */
+  app?: Hono
+}
+
+export function $bootstrapServerCore({
+  abortController,
+  port = 8080,
+  app = new Hono()
+}: ForInitiableServer) {
+  try {
+    const signal = abortController.signal
+
+    Deno.serve(
+      {
+        port,
+        signal
+      },
+      app.fetch
+    )
+  } catch (err: unknown) {
+    const parseError = err as Error
+    console.error(parseError.message)
+    abortController.abort()
+  }
+}
--- src/mod.ts
+++ src/mod.ts
@@ -1,10 +1,17 @@
-import { Hono } from "https://deno.land/x/hono@v3.9.2/mod.ts"
+import { Hono } from 'npm:hono'
+import { PORT } from './config/env.config.ts'
+import { $bootstrapServerCore } from './core/bootstrap-server.core.ts'
 
 const app = new Hono()
 
-app.get('/', ctx => {
-  return ctx.text('Hello World')
+const port = PORT
+const abortController = new AbortController()
+
+app.get('/', c => c.json({ server: 'hono' }))
+app.get('/health', c => c.json({ status: 200, message: 'OK' }))
+
+$bootstrapServerCore({
+  app,
+  port,
+  abortController
 })
-
-
-Deno.serve(app.fetch)
