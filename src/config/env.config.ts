6c026a0e9c7bf22acb4c96bf151acb79e5b058aa
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-11-07 22:21:41 +0000

    Add PORT to env config

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,11 +1,13 @@
 import { load } from '$std/dotenv/mod.ts'
 
 const envs = {
-  API_VERSION_PREFIX: 'API_VERSION_PREFIX'
+  API_VERSION_PREFIX: 'API_VERSION_PREFIX',
+  PORT: 'PORT'
 }
 
 const defaultEnvs = {
-  API_VERSION_PREFIX: 'v1'
+  API_VERSION_PREFIX: 'v1',
+  PORT: 8080
 }
 
 const envsKeys = Object.keys(envs) as (keyof typeof envs)[]
@@ -13,4 +15,6 @@ const envsKeys = Object.keys(envs) as (keyof typeof envs)[]
 export const env = (await load()) as Record<(typeof envsKeys)[number], string>
 
 export const API_VERSION_PREFIX =
-  env['API_VERSION_PREFIX'] ?? defaultEnvs['API_VERSION_PREFIX']
+  env.API_VERSION_PREFIX ?? defaultEnvs.API_VERSION_PREFIX
+
+export const PORT = env.PORT ?? defaultEnvs.PORT
