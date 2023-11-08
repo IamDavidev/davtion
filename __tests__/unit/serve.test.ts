ce733753d6fb1f90cc2ce3ea8e7ba9c76d4c0133
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-11-08 19:40:45 +0000

    Add API endpoint for updating image

--- __tests__/unit/serve.test.ts
+++ __tests__/unit/serve.test.ts
@@ -1,35 +1,18 @@
+/**
+ * Load libraries
+ */
 import { testClient } from 'npm:hono/testing'
 import { ValidationTargets } from 'npm:hono'
 import { Env, Hono, ToSchema } from 'npm:hono'
-
 import { describe, it } from '$std/testing/bdd.ts'
 import { assertEquals } from '$std/assert/assert_equals.ts'
+
+/**
+ * Load internal modules
+ */
 import { $loadRoutesCore } from '@core/load-routes.core.ts'
 
-export type ApiValidation = Partial<ValidationTargets>
-
-export type ServerType = Hono<Env>
-
-it('Example ', async () => {
-  /**
-   * @Given
-   */
-  const data = { user: 'user', password: 'password' }
-
-  type Route = ToSchema<'get', '/search', ApiValidation, typeof data>
-  /**
-   * @When
-   */
-  const app = new Hono()
-
-  app.get('/search', c => c.json(data))
-
-  const res = await testClient(app as Hono<Env, Route, '/'>).search.$get()
-
-  const resJson = await res.json()
-
-  assertEquals(resJson, data)
-})
+type ApiValidation = Partial<ValidationTargets>
 
 describe('Api health', () => {
   it('Should return status 200', async () => {
@@ -53,6 +36,41 @@ describe('Api health', () => {
     /**
      * @Then
      */
+    assertEquals(res.status, 200)
+  })
+})
+
+describe('Api Update Image', () => {
+  it('Should return image id', async () => {
+    /**
+     * @Given
+     */
+    const expected = { status: 200, message: 'OK' }
+    const app = new Hono() as HonoWithUpdateImage
+
+    /**
+     * Parse types for warnings
+     */
+    type UpdateImageSchemaApi = ToSchema<
+      'get',
+      `/api/v100/update-image`,
+      ApiValidation,
+      typeof expected
+    >
+
+    type HonoWithUpdateImage = Hono<Env, UpdateImageSchemaApi, '/'>
+
+    /**
+     * @When
+     */
+    $loadRoutesCore({ app })
+    const res = await testClient(app).api.v100['update-image'].$get()
+    const resJson = await res.json()
+    /**
+     * @Then
+     */
+
+    assertEquals(resJson, expected)
     assertEquals(res.status, 200)
   })
 })
