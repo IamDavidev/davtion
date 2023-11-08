499392b0bf3c0636bbdc975c70b56d72e436aa9d
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-11-08 19:40:57 +0000

    Add UpdateImageController and logger middleware

--- src/core/load-routes.core.ts
+++ src/core/load-routes.core.ts
@@ -1,4 +1,7 @@
-import { Hono } from 'npm:hono'
+import { Context, Hono } from 'npm:hono'
+import { logger } from 'npm:hono/logger'
+
+import { API_VERSION_PREFIX } from '@config/env.config.ts'
 
 interface ForLoaderRoutes {
   /**
@@ -7,7 +10,43 @@ interface ForLoaderRoutes {
   app: Hono
 }
 
+export abstract class Controller {
+  private readonly API = '/api'
+  private readonly PREFIX = `${this.API}/${API_VERSION_PREFIX}`
+
+  constructor(private readonly app: Hono) {}
+
+  protected abstract path: string
+
+  protected abstract handler(ctx: Context): Response
+
+  private genPath() {
+    return `${this.PREFIX}${this.path}`
+  }
+
+  private printPath(path: string) {
+    console.log(`[INFO] ${path}`)
+  }
+
+  public load() {
+    const path = this.genPath()
+    this.printPath(path)
+    this.app.get(path, this.handler.bind(this))
+  }
+}
+
+export class UpdateImageController extends Controller {
+  protected path = '/update-image'
+
+  protected handler(ctx: Context) {
+    return ctx.json({ status: 200, message: 'OK' })
+  }
+}
+
 export function $loadRoutesCore({ app }: ForLoaderRoutes) {
-  app.get('/', c => c.json({ server: 'hono' }))
+  app.use('*', logger())
+  new UpdateImageController(app).load()
+
+  app.get('/', c => c.json({ server: 'Davtion Api' }))
   app.get('/health', c => c.json({ status: 200, message: 'OK' }))
 }
