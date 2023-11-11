d3de7af1407d0b670459a876471b9a4dc1616e44
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-11-11 01:15:48 +0000

    Refactor image upload route in routes.core.ts

--- src/core/routes.core.ts
+++ src/core/routes.core.ts
@@ -1,9 +1,5 @@
-import { API_VERSION_PREFIX } from '@config/env.config.ts'
-
-export const genRoute = (path: string) => `/api/${API_VERSION_PREFIX}/${path}`
-
 export const ROUTES = {
   IMAGE: {
-    UPLOAD: genRoute('image-upload')
+    UPLOAD: 'upload-image'
   }
 }
