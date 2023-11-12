705a527dc4a4a571acb84033292ec115e5be9d9b
IamDavidev <davidlez2mgk@gmail.com>
2023-11-12 10:38:53 -0600

    remove example tests

--- __tests__/unit/mock.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it } from '$std/testing/bdd.ts'
-import { assertEquals } from '$std/assert/mod.ts'
-
-describe('mock test', () => {
-  it('should be true', () => {
-    const result = true
-    assertEquals(result, true)
-  })
-})
