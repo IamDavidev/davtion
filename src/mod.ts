ddb7186059b9ff1f394aa7373ab4066ad48b6f0d
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-11-14 04:54:57 +0000

    Add middleware loader to server setup

--- src/mod.ts
+++ src/mod.ts
@@ -3,12 +3,14 @@ import { Hono } from 'npm:hono'
 import { PORT } from '@config/env.config.ts'
 import { $bootstrapServerCore } from '@core/bootstrap-server.core.ts'
 import { $loadRoutesCore } from '@core/load-routes.core.ts'
+import { $loadMiddlewaresCore } from '@core/load-middlewares.core.ts'
 
 const app = new Hono()
 
 const port = PORT
 const abortController = new AbortController()
 
+$loadMiddlewaresCore({ app })
 $loadRoutesCore({ app })
 
 $bootstrapServerCore({
